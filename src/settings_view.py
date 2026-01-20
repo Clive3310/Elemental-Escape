ad873349aa5a027f1381e8ab2c04e657353046ec
arrte <arrteem41@gmail.com>
2026-01-20 19:13:32 +0300

    Update menu view for fullscreen support, save settings to json permanently

--- src/settings_view.py
+++ src/settings_view.py
@@ -1,5 +1,6 @@
 import arcade.gui
-
+import json
+import pathlib
 from src.constants import *
 
 MAX_BUTTON_SIZE = (400, 300)
@@ -12,13 +13,13 @@ class SettingsView(arcade.View):
     def __init__(self):
         super().__init__()
         self.background_color = WINDOW_MENU_COLOR
-        self.ui_manager = arcade.gui.UIManager()
+        self.UIman = arcade.gui.UIManager()
         self.title_text = None
         self.setup()
 
     def setup(self):
-        self.ui_manager.enable()
-        self.ui_manager.clear()
+        self.UIman.enable()
+        self.UIman.clear()
 
         self.title_text = arcade.Text(
             "Settings",
@@ -54,7 +55,7 @@ class SettingsView(arcade.View):
         v_box.add(btn_back)
 
         anchor_layout.add(child=v_box, anchor_x="center_x", anchor_y="center_y")
-        self.ui_manager.add(anchor_layout)
+        self.UIman.add(anchor_layout)
 
     def on_click_fullscreen(self, event):
         self.window.set_fullscreen(not self.window.fullscreen)
@@ -64,8 +65,25 @@ class SettingsView(arcade.View):
         else:
             self.btn_fullscreen.text = "Fullscreen: OFF"
             self.window.set_size(int(WINDOW_SIZE[0]), int(WINDOW_SIZE[1]))
+
         self.title_text.x = self.window.width / 2
         self.title_text.y = self.window.height / 2 + WINDOW_SIZE[1] // 4
+
+        path = pathlib.Path(__file__).parent / "settings.json"
+
+        try:
+            data = {}
+            if path.exists():
+                with open(path, "r", encoding="utf-8") as f:
+                    data = json.load(f)
+
+            data["fullscreen"] = self.window.fullscreen
+
+            with open(path, "w", encoding="utf-8") as f:
+                json.dump(data, f, indent=4)
+
+        except Exception as e:
+            print(f"Ошибка сохранения настроек: {e}")
 
     def on_click_back(self, event):
         from src.menu_view import MenuView
@@ -73,13 +91,13 @@ class SettingsView(arcade.View):
         self.window.show_view(view)
 
     def on_show_view(self):
-        self.ui_manager.enable()
+        self.UIman.enable()
 
     def on_hide_view(self):
-        self.ui_manager.disable()
+        self.UIman.disable()
 
     def on_draw(self):
         self.clear()
-        self.ui_manager.draw()
+        self.UIman.draw()
         if self.title_text:
             self.title_text.draw()
